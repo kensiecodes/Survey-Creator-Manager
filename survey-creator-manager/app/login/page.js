a00225e9328a6f36d7187ed62c360ca08dfcaa5c
Emeric FOKOUONG TOUOLAC <bestvemeric@gmail.com>
2023-09-18 14:48:22 -0500

    added usestate to get login inforation from the form

--- survey-creator-manager/app/login/page.js
+++ survey-creator-manager/app/login/page.js
@@ -1,32 +1,65 @@
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+
 export default function Page() {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`Username: ${userName} \nPassword: ${password}`);
+    setPassword("");
+    setUserName("");
+  };
   return (
-    <div className="container">
+    <form onSubmit={handleSubmit} className="container">
       <div className="mb-5 text-center">
         {/* this is a test commit message */}
         <h2>Login</h2>
       </div>
       <div className="input-group mb-4">
-        <label htmlFor="userName" className="block mb-2">Username:</label>
-        <input type="text" id="userName" name="userName" className="input"/>
+        <label htmlFor="userName" className="block mb-2">
+          Username:
+        </label>
+        <input
+          type="text"
+          id="userName"
+          name="userName"
+          className="input"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+        />
       </div>
       <div className="input-group">
         <label htmlFor="passwordInput">Password:</label>
-        <input type="password" id="passwordInput" name="passwordInput" className="input"/>
+        <input
+          type="password"
+          id="passwordInput"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          name="passwordInput"
+          className="input"
+        />
       </div>
       <div className="submit-group">
-        <button type="submit" className="mb-4">Login</button>
+        <button type="submit" className="mb-4">
+          Login
+        </button>
         <br />
         <p>
-          Don't have an account? <a href="/signup" className="text-black underline">Sign up</a>
+          Don't have an account?{" "}
+          <Link href="/signup" className="text-black underline">
+            Sign up
+          </Link>
         </p>
         <p>
-          Forgot your password? <a href="/reset" className="text-black underline">Reset password</a>
+          Forgot your password?{" "}
+          <Link href="/reset" className="text-black underline">
+            Reset password
+          </Link>
         </p>
       </div>
-
-
-
-    </div>
-  )
+    </form>
+  );
 }
-
